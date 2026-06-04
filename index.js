9ab1a49c4856c4c90f3c6866dd03a503c0aff5c8
Davis Sebastian <davis.sebastian14@gmail.com>
2026-06-04 19:33:37 +0530

    feat: all routes connected to PostgreSQL

--- index.js
+++ index.js
@@ -14,37 +14,45 @@ app.get('/users', async(req, res)=> {
     }
 });
 
-app.get('/users/:id', function(req, res) {
-    const id = parseInt(req.params.id);
-    const user= users.find(function(u) {
-        return u.id === id;
-    });
-    if(!user) {
-        return res.status(404).json({message: 'User not found' });
+app.get('/users/:id', async(req, res)=> {
+    try{
+         const id = parseInt(req.params.id);
+         const result= await pool.query(`SELECT * FROM users where id = $1`,[id]);
+         if(result.rows.length === 0){
+            return res.status(404).json({error: 'User not found!'});
+         }
+         res.json(result.rows[0]);
+
+    } catch(err){
+        console.error(err.message);
+        res.status(500).json({error: 'Server error'});
     }
-    res.json(user);
 });
 
-app.post('/users', function(req, res){
-    const newUser = {
-        id: users.length + 1,
-        name: req.body.name,
-        role: req.body.role
-    };
-    users.push(newUser);
-    res.status(201).json(newUser);
+app.post('/users', async(req, res)=>{
+    try{
+        const name= req.body.name;
+        const role= req.body.role;
+        const newUser= await pool.query(`INSERT INTO users (name,role) VALUES ($1,$2) RETURNING *`,[name, role]);
+        res.status(201).json(newUser.rows[0]);
+    } catch (err){
+        console.error(err.message);
+        res.status(500).json({error: 'Database error'});
+    }
 });
 
-app.delete('/users/:id', function(req, res){
-   const id= parseInt(req.params.id);
-   const index= users.findIndex(function(u){
-        return u.id === id;
-   });
-   if(index === -1){
-        return res.status(404).json({message: 'User not foound'});
-   }
-   const deletedUser = users.splice(index, 1)[0];
-   res.json(deletedUser); 
+app.delete('/users/:id', async(req, res)=>{
+    try{
+        const id= parseInt(req.params.id);
+        const deletedUser= await pool.query(`DELETE FROM users WHERE id = $1 RETURNING *`,[id]);
+        if(deletedUser.rows.length === 0){
+            return res.status(404).json({error: 'User not found'})
+        }
+        res.json(deletedUser.rows[0]);
+    } catch (err){
+        console.error(err.message);
+        res.status(500).json({error: 'Database error'})
+    } 
 })
 
 app.listen(3000, function() {
